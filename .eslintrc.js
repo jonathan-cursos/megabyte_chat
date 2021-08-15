195af2b1c07c9e77addd8dc3f415eb9eb36f05be
Jonathan García <jona03g97@gmail.com>
2021-08-14 19:43:33 -0600

    Config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -13,5 +13,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react'],
-  rules: {}
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
 }
