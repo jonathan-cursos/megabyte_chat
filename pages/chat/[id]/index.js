97fb9547a24fe981333e29cae016128b08e9a327
Jonathan García <megabyte9703@gmail.com>
2021-08-21 17:43:04 -0600

    Render users of each on each chat

--- pages/chat/[id]/index.js
+++ pages/chat/[id]/index.js
@@ -1,15 +1,47 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Layout from '../../../components/Layout'
+import styles from './styles'
 
-const Chat = ({chats}) => {
+const Chat = ({ chats, userId }) => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const chatsUsers = chats.map((chat) => {
+      const chatReceptor = chat.users.filter((user) => {
+        let userItem = {} // Para que el lint no diera problemas al no retornar nada del filter
+        if (user._id !== userId) {
+          userItem = user
+        }
+        return userItem
+      })
+      return chatReceptor[0]
+    })
+    setUsers(chatsUsers)
+  }, [])
+
   return (
     <>
-    <Head>
-      <title>Megabyte Chat - Chats</title>
-    </Head>
+      <Head>
+        <title>Megabyte Chat - Chats</title>
+      </Head>
       <Layout>
-        {console.log(chats)}
+        <ul>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <li key={user._id}>
+                <Link href='/'>
+                  <a>{user.name}</a>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <p>No se encontraron chats. Crea uno nuevo</p>
+          )}
+        </ul>
       </Layout>
+      <style jsx>{styles}</style>
     </>
   )
 }
@@ -17,10 +49,10 @@ const Chat = ({chats}) => {
 Chat.getInitialProps = (ctx) => {
   const userId = ctx.query.id
   return fetch(`http://localhost:3001/chat/${userId}`)
-  .then(res => res.json())
-  .then(({body: chats}) => {
-    return {chats}
-  })
+    .then((res) => res.json())
+    .then(({ body: chats }) => {
+      return { chats, userId }
+    })
 }
 
 export default Chat
---
+++ pages/chat/[id]/styles.js
@@ -0,0 +1,20 @@
+import css from 'styled-jsx/css'
+
+export default css`
+  p {
+    color: white;
+    font-size: 1.6rem;
+  }
+
+  ul {
+    width: 100%;
+    max-width: 300px;
+    height: 130px;
+    overflow-y: auto;
+  }
+
+  a {
+    color: white;
+    font-size: 1.6rem;
+  }
+`
