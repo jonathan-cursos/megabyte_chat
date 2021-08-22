c58623de0b1ae18f68dc9e260414d9c446806da3
Jonathan García <megabyte9703@gmail.com>
2021-08-22 09:46:49 -0600

    Delete style-in-jsx of pages

--- pages/chat/[id]/index.js
+++ pages/chat/[id]/index.js
@@ -29,7 +29,24 @@ const Chat = ({ chats, userId }) => {
           })}
         </ul>
       </Layout>
-      <style jsx>{styles}</style>
+      <style jsx>{`
+        p {
+          color: white;
+          font-size: 1.6rem;
+        }
+
+        ul {
+          width: 100%;
+          max-width: 300px;
+          height: 130px;
+          overflow-y: auto;
+        }
+
+        a {
+          color: white;
+          font-size: 1.6rem;
+        }
+      `}</style>
     </>
   )
 }
--- pages/user/index.js
+++ pages/user/index.js
@@ -21,7 +21,25 @@ const User = ({ users }) => {
           })}
         </ul>
       </Layout>
-      <style jsx>{styles}</style>
+      <style jsx>{`
+        ul {
+          width: 100%;
+          max-width: 300px;
+          height: 130px;
+          overflow-y: auto;
+        }
+
+        li {
+          height: 30px;
+          display: flex;
+          align-items: center;
+        }
+
+        p {
+          color: ${colors.primary};
+          font-size: 1.4rem;
+        }
+      `}</style>
     </>
   )
 }
