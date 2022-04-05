523d04ad7c87b98c3fe0ce4244c70f5b7ccebb66
Jonathan García <megabyte9703@gmail.com>
2022-04-04 20:39:08 -0600

    Fix: data returned in useGetData

--- hooks/useGetData.js
+++ hooks/useGetData.js
@@ -28,7 +28,7 @@ const useGetData = ({ userId = '' } = {}) => {
   const { data, error } = useSwr(endpoint, fetcher)
 
   return {
-    data: data || [],
+    data: data,
     isLoading: !error && !data,
     isError: error
   }
