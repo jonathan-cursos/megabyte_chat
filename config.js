163b766c08852b0eea72cd0f238f5492bdb98a7c
Jonathan García <jona03g97@gmail.com>
2023-01-03 22:23:55 -0600

    Fix: Wrong

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const API = 'https://megabyte-chat-websockets.onrender.com'
+export const API = 'https://megabyte-chat-backend.onrender.com'
 export const WS_SRV = process.env.NEXT_PUBLIC_WS_SERVER
